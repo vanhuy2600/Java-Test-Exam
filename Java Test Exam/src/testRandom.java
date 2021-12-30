7d1ab4753af7fe68693e202e36cb4260e9a5358d
Van Huy <vanhuy005xtdl@gmail.com>
2021-12-30 22:14:03 +0700

    pushCode

---
+++ Java Test Exam/src/testRandom.java
@@ -0,0 +1,20 @@
+
+import java.util.Random;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class testRandom {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        boolean v = rand.nextInt(10) == 0;
+        System.out.println(v);
+    } 
+}
